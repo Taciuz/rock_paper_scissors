4c0d741d555bac2d5410a528dbbde8aeee52cf10
Taciuz <stefano.tacelli@gmail.com>
2024-01-14 01:53:11 +0100

    Fixed logic with button

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
+const rockButton = document.getElementById("rockButton");
+const paperButton = document.getElementById("paperButton");
+const scissorsButton = document.getElementById("scissorsButton");
+let usrScore = document.getElementById("usrScore");
+let cpuScore = document.getElementById("cpuScore");
+let playerScore = 0;
+let computerScore = 0;
+
 const valueChoice = ["Rock", "Paper", "Scissors"];
 
 function getComputerChoice(){
     return valueChoice[Math.floor(Math.random() * valueChoice.length)];
 }
 
-let userScore = 0;
-let computerScore = 0;
+rockButton.addEventListener('click', () => handleClick("Rock"));
+paperButton.addEventListener('click', () => handleClick("Paper"));
+scissorsButton.addEventListener('click', () => handleClick("Scissors"));
+
+function handleClick(playerSelection){
+    playRound(playerSelection, getComputerChoice());
+}
 
 function playRound(userSelection, computerSelection){
-    userSelection = prompt("What is your choice?");
-    computerSelection = getComputerChoice();
     if (userSelection == computerSelection){
         console.log('It\'s a tie!');
     }
@@ -17,21 +28,13 @@ function playRound(userSelection, computerSelection){
             (userSelection == "Scissors" && computerSelection == "Paper") ||
             (userSelection == "Paper" && computerSelection == "Rock")){
         console.log("User win!");
-        console.log(userSelection)
-        console.log(computerSelection);
-        userScore++;
+        playerScore++;
+        usrScore.textContent = `${playerScore}`;
     }
     else {
         console.log("Computer wins");
         computerScore++;
+        cpuScore.textContent = `${computerScore}`;
     }
     return {userSelection, computerSelection};
-}
-
-function game(){
-    while(userScore < 5 || computerScore<=5){
-    playRound();
-    console.log("User Score=" + userScore);
-    console.log("Computer Score:" + computerScore);
-    }
 }
